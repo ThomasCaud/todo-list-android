e4cf5c2cc70bb16d4e2958d8c9e2ddb69b048134
Thomas Caudrelier <thomas.caudrelier@etu.utc.fr>
2018-11-16 11:48:28 +0100

    Add statut field in creation, improve design

--- app/src/main/java/com/example/thomas/tp2/AddTaskActivity.java
+++ app/src/main/java/com/example/thomas/tp2/AddTaskActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.thomas.tp2.model.Task;
 
@@ -13,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class AddTaskActivity extends AppCompatActivity {
-
     private void goToMainPage(ArrayList<Task> tasks) {
         Intent newIntent = new Intent(AddTaskActivity.this, MainActivity.class);
         Bundle args = new Bundle();
@@ -42,6 +44,13 @@ public class AddTaskActivity extends AppCompatActivity {
     private void initializeFields() {
         EditText label = findViewById(R.id.etLabel);
         label.setText("");
+
+        RadioButton rbTodo = findViewById(R.id.rbTodo);
+        RadioButton rbDoing = findViewById(R.id.rbDoing);
+        RadioButton rbDone = findViewById(R.id.rbDone);
+        rbTodo.setChecked(false);
+        rbDoing.setChecked(false);
+        rbDone.setChecked(false);
     }
 
     private void setClearButtonListener() {
@@ -54,18 +63,52 @@ public class AddTaskActivity extends AppCompatActivity {
         });
     }
 
+    private boolean isValidTask() {
+        boolean isValid = true;
+        EditText label = findViewById(R.id.etLabel);
+        if(label.getText().toString().length() == 0) {
+            Toast.makeText(getApplicationContext(), "Le label doit être renseigné", Toast.LENGTH_LONG).show();
+            isValid = false;
+        } else {
+            RadioGroup rgStatus = findViewById(R.id.rgStatus);
+            if(rgStatus.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(getApplicationContext(), "Le statut doit être renseigné", Toast.LENGTH_LONG).show();
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    }
+
+    private Task.STATUS getCheckedStatut() {
+        RadioButton rbTodo = findViewById(R.id.rbTodo);
+        if(rbTodo.isChecked()) {
+            return Task.STATUS.TODO;
+        } else {
+            RadioButton rbDoing = findViewById(R.id.rbDoing);
+            if(rbDoing.isChecked()) {
+                return Task.STATUS.DOING;
+            } else {
+                return Task.STATUS.DONE;
+            }
+        }
+    }
+
     private void setCreateButtonListener() {
         Button button = findViewById(R.id.create);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText label = findViewById(R.id.etLabel);
-                Task newTask = new Task(label.getText().toString(), Task.STATUS.TODO, Task.PRIORITY.LOW, new Date());
+                if(isValidTask()) {
+                    EditText label = findViewById(R.id.etLabel);
 
-                ArrayList<Task> tasks = getTasks();
-                tasks.add(newTask);
+                     Task newTask = new Task(label.getText().toString(), getCheckedStatut(), Task.PRIORITY.LOW, new Date());
 
-                goToMainPage(tasks);
+                    ArrayList<Task> tasks = getTasks();
+                    tasks.add(newTask);
+
+                    goToMainPage(tasks);
+                }
             }
         });
     }
